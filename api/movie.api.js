b5391dcdae14b78d066bf29d6a18e462b6cfab7d
Niinyah <petermarbell@gmail.com>
2026-03-13 11:42:38 +0100

    Adminmovielist dont

--- api/movie.api.js
+++ api/movie.api.js
@@ -23,4 +23,46 @@ export async function fetchMovie(movieId) {
   } catch (error) {
     console.error("Error fetching movie", error);
   }
+
+  
+}
+
+export async function updateMovie(movieId, movieData) {
+  try {
+    const response = await fetch(`${BASE_URL_MOVIE}/${movieId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(movieData),
+    });
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    return await response.json();
+  } catch (error) {
+    console.error("Error updating movie", error);
+  }
+}
+
+export async function deleteMovie(movieId) {
+  try {
+    const response = await fetch(`${BASE_URL_MOVIE}/${movieId}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    return true;
+  } catch (error) {
+    console.error("Error deleting movie", error);
+  }
+}
+
+export async function createMovie(movieData) {
+  try {
+    const response = await fetch(BASE_URL_MOVIE, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(movieData),
+    });
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    return await response.json();
+  } catch (error) {
+    console.error("Error creating movie", error);
+  }
 }
